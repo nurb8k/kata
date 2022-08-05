2cb2039027c42e7e9ce28506c7a8c9c9cc277c66
nurb8k <nurb4k@gmail.com>
2022-08-05 15:52:55 +0600

    Changed

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
     public static String calc(String input) {
 
@@ -55,6 +59,12 @@ public class Main {
         return "Error";
     }
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(calc(str));
+    }
 
 
 
